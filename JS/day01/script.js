32b7098911508d2c4b88e4b8af0d2800e9e44c70
PiyushChaturvedii <piyush1.chatur@gmail.com>
2024-08-06 19:48:53 +0530

    day01

---
+++ JS/day01/script.js
@@ -0,0 +1,23 @@
+let abc = 5;
+// if (abc === 5) alert('Hello World');
+console.log(40 + 18 + 9 + 6 - 78);
+console.log("Shri Ram");
+console.log("1234");
+
+let firstName = "Sita";
+// firstNamePerson;
+// first_name;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
