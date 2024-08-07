2531498c5de16a68a1369a747be92d0bf34b44a7
PiyushChaturvedii <piyush1.chatur@gmail.com>
2024-08-07 19:44:44 +0530

    DAY02
    Signed-off-by: PiyushChaturvedii <piyush1.chatur@gmail.com>

---
+++ JS/day02/script.js
@@ -0,0 +1,59 @@
+/*
+let abc = 5;
+// if (abc === 5) alert('Hello World');
+console.log(40 + 18 + 9 + 6 - 78);
+console.log("Shri Ram");
+console.log("1234");
+
+// Variable name conventions
+let firstName = "Sita";
+// firstNamePerson;
+// first_name;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+*/
+
+// let bool = true;
+// console.log(bool);
+// let str = "true";
+// // console.log(str);
+
+// // console.log(typeof true);
+// console.log(typeof bool);
+// console.log(typeof str);
+// // console.log(typeof 23);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1993
+// console.log(typeof year);
+
+
+// console.log(typeof null);
+
+let name;
+let age = 30;
+age = 31;
+
+const birthYear = 1988;
+// birthYear = 1993;
+// const job = 'teacher';
+
+var job = 'teacher';
+job = 'programmer';
+
+lastName = 'Sharma';
+console.log(lastName);
