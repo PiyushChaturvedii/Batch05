f5f56de3a21e628e691f97c114df0deabe81b950
PiyushChaturvedii <piyush1.chatur@gmail.com>
2024-08-08 20:27:56 +0530

    day03

---
+++ JS/day03/script.js
@@ -0,0 +1,114 @@
+// // /*
+// // let abc = 5;
+// // // if (abc === 5) alert('Hello World');
+// // console.log(40 + 18 + 9 + 6 - 78);
+// // console.log("Shri Ram");
+// // console.log("1234");
+
+// // // Variable name conventions
+// // let firstName = "Sita";
+// // // firstNamePerson;
+// // // first_name;
+
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+
+// // let myFirstJob = 'Programmer';
+// // let myCurrentJob = 'Teacher';
+
+// // let job1 = 'programmer';
+// // let job2 = 'teacher';
+// // */
+
+// // // let bool = true;
+// // // console.log(bool);
+// // // let str = "true";
+// // // // console.log(str);
+
+// // // // console.log(typeof true);
+// // // console.log(typeof bool);
+// // // console.log(typeof str);
+// // // // console.log(typeof 23);
+
+// // // let year;
+// // // console.log(year);
+// // // console.log(typeof year);
+
+// // // year = 1993
+// // // console.log(typeof year);
+
+
+// // // console.log(typeof null);
+
+// // let name;
+// // let age = 30;
+// // age = 31;
+
+// // const birthYear = 1988;
+// // // birthYear = 1993;
+// // // const job = 'teacher';
+
+// // var job = 'teacher';
+// // job = 'programmer';
+
+// // lastName = 'Sharma';
+// // console.log(lastName);
+
+// // Math Operators
+// const now = 2027;
+// const ageRam = now - 1988;
+// const ageSita = now - 2009;
+// console.log(ageRam, ageSita);
+// // console.log(ageRam);
+// // console.log(ageSita);
+
+
+// console.log(ageRam * 2, ageRam / 10, 2 * 3, 2 ** 3);
+// //2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// //concat()
+
+// const firstName = 'Ram';
+// const lastName = 'Sharma';
+// console.log(firstName + ' ' + lastName);
+
+// //Assignment operators
+// let x = 10 + 5; // 15
+// // x = x + 10;
+// x += 10;
+// x *= 4;// x = x * 4 = 100
+// x++; // x = x + 1;
+// x--;// x = x - 1;
+// x--;
+// console.log(x);
+
+// // Comparison operators
+
+// console.log(ageRam > ageSita); // > , <, <=, >=
+
+// console.log(ageSita >= 18);
+
+// const isFullAge = ageSita >= 18;
+
+
+// console.log(now - 1991 > now - 2018);
+
+
+const now = 2024;
+const ageRam = now - 1988;
+const ageSita = now - 2018;
+
+
+console.log(now - 1988 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+console.log(ageRam, ageSita);
+const averageAge = (ageRam + ageSita) / 2;
+console.log(averageAge);
