de942c9be48f4c416564a08861a3f84fc0caa8e3
PiyushChaturvedii <piyush1.chatur@gmail.com>
2024-08-09 19:57:57 +0530

    day04

---
+++ JS/day04/script.js
@@ -0,0 +1,194 @@
+// // /*
+// // let abc = 5;
+// // // if (abc === 5) alert('Hello World');
+// // console.log(40 + 18 + 9 + 6 - 78);
+// // console.log("Shri Ram");
+// // console.log("1234");
+
+// // // Variable name conventions
+// // let firstName = "Sita";
+// // // firstNamePerson;
+// // // first_name;
+
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+
+// // let myFirstJob = 'Programmer';
+// // let myCurrentJob = 'Teacher';
+
+// // let job1 = 'programmer';
+// // let job2 = 'teacher';
+// // */
+
+// // // let bool = true;
+// // // console.log(bool);
+// // // let str = "true";
+// // // // console.log(str);
+
+// // // // console.log(typeof true);
+// // // console.log(typeof bool);
+// // // console.log(typeof str);
+// // // // console.log(typeof 23);
+
+// // // let year;
+// // // console.log(year);
+// // // console.log(typeof year);
+
+// // // year = 1993
+// // // console.log(typeof year);
+
+
+// // // console.log(typeof null);
+
+// // let name;
+// // let age = 30;
+// // age = 31;
+
+// // const birthYear = 1988;
+// // // birthYear = 1993;
+// // // const job = 'teacher';
+
+// // var job = 'teacher';
+// // job = 'programmer';
+
+// // lastName = 'Sharma';
+// // console.log(lastName);
+
+// // Math Operators
+// const now = 2027;
+// const ageRam = now - 1988;
+// const ageSita = now - 2009;
+// console.log(ageRam, ageSita);
+// // console.log(ageRam);
+// // console.log(ageSita);
+
+
+// console.log(ageRam * 2, ageRam / 10, 2 * 3, 2 ** 3);
+// //2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// //concat()
+
+// const firstName = 'Ram';
+// const lastName = 'Sharma';
+// console.log(firstName + ' ' + lastName);
+
+// //Assignment operators
+// let x = 10 + 5; // 15
+// // x = x + 10;
+// x += 10;
+// x *= 4;// x = x * 4 = 100
+// x++; // x = x + 1;
+// x--;// x = x - 1;
+// x--;
+// console.log(x);
+
+// // Comparison operators
+
+// console.log(ageRam > ageSita); // > , <, <=, >=
+
+// console.log(ageSita >= 18);
+
+// const isFullAge = ageSita >= 18;
+
+
+// console.log(now - 1991 > now - 2018);
+
+
+// const now = 2024;
+// const ageRam = now - 1988;
+// const ageSita = now - 2018;
+
+
+// console.log(now - 1988 > now - 2018);
+
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// console.log(ageRam, ageSita);
+// const averageAge = (ageRam + ageSita) / 2;
+// console.log(averageAge);
+
+// const firstName = 'Ram';
+// const lastName = 'Sharma';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2024;
+
+// const ram = "I'm " + firstName +' '+ lastName + ', a ' + (year - birthYear) + ' year old ' + job + ' !';
+
+// console.log(ram);
+
+// const ramNew = `I'm ${firstName} ${lastName}, a ${year - birthYear} year old ${job} !`;
+
+// console.log(ramNew);
+
+// console.log(`Just a regular string...`);
+
+
+// console.log('String with \n\
+//     multiple \n\
+//     lines');
+
+// console.log(`String with
+//         multiple
+//         lines.`);
+
+// const age = 18;
+// const isOldEnough = age >= 18;
+
+// if (isOldEnough) {
+//     console.log('Start driving License 🏎🚗🚓🚕')
+// }
+// const age = 12;
+// if (age>=18) {
+//     console.log('Start driving License 🏎🚗🚓🚕')
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Too young, Wait another ${yearsLeft} years.`);
+// }
+
+// const birthYear = 2001;
+
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+
+// console.log(century);
+
+// const inputYear = '1991';
+// console.log(inputYear + 10);
+// console.log(Number(inputYear), inputYear);
+
+// console.log(Number('Ram'));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+
+// type coercion
+// console.log('I am ' + 23 + ' year old.');
+
+// console.log('23' - '10' - 3);
+
+// console.log('22' / '2');
+
+
+let n = '1' + 1;
+n = n - 1;
+
+console.log(n);
+
+let a = 2 + 3 + 4 + '5';
+
+console.log(a);
+
+let b = '10' - '4' - '3' - 2 + '5';
+console.log(b);
