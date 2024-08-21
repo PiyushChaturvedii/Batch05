908ba404d088ae6a44a81bc99b0ab586dadd493c
PiyushChaturvedii <piyush1.chatur@gmail.com>
2024-08-21 19:58:55 +0530

    day09

---
+++ JS/day09/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+let vote = false;
+const novote = true;
+
+if (novote) vote = true;
+if(vote) console.log("You can vote.");
+
+// const interface = 'ram';
+const private = 'ram';
