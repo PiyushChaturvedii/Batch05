e8629cb84535bf20ae1c435bb3bfdd915fad027b
PiyushChaturvedii <piyush1.chatur@gmail.com>
2024-09-09 20:33:16 +0530

    day19

---
+++ JS/day018/script.js
@@ -0,0 +1,55 @@
+// console.log("I am go to School. day 1");
+// console.log("I am go to School. day 2");
+// console.log("I am go to School. day 3");
+// console.log("I am go to School. day 4");
+// console.log("I am go to School. day 5");
+// console.log("I am go to School. day 6");
+// console.log("I am go to School. day 7");
+// console.log("I am go to School. day 8");
+// console.log("I am go to School. day 9");
+// console.log("I am go to School. day 10");
+
+
+// FOR loop keeps running while condition is TRUE
+
+// for (let x = 1; x <= 10; x+=2){
+//     console.log(`I am go to School. day ${x}`);
+// }
+
+const RamArray = [
+    'Ram',
+    'Sharma',
+    2024 - 1991,
+    'teacher',
+    ['rohan', 'mohan', 'sohan'],
+  
+]
+const types = [];
+
+// console.log(RamArray[0]);
+// console.log(RamArray[1]);
+// console.log(RamArray[2]);
+// console.log(RamArray[3]);
+// console.log(RamArray[4]);
+
+// for (let i = 0; i < RamArray.length; i++){
+//     console.log(RamArray[i], typeof RamArray[i]);   
+
+//     types[i] =  typeof RamArray[i];
+
+// }
+
+// console.log(types);
+
+const years = [1991, 1987, 1956, 1974, 2022,2004,2006];
+const age = [];
+
+for (let i = 0; i < years.length; i++){
+    age.push(2024 - years[i]);
+}
+
+console.log(age);
+
+
+
+
