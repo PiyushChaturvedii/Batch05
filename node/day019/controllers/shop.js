2eb16893473a8659da2244dc86ea813443fac035
PiyushChaturvedii <piyush1.chatur@gmail.com>
2025-02-17 21:40:38 +0530

    producterror

--- node/day019/controllers/shop.js
+++ node/day019/controllers/shop.js
@@ -10,16 +10,16 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getProduct =(req,res,next)=>{
+exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId,product =>{
-    res.render('shop/product-detail',{
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
       product: product,
-    pageTitle: product.title,
-  path:'/products'
-});
-  })
-}
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -37,13 +37,6 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
-
-exports.postCart = (req,res,next)=>{
-  const prodId = req.body.productId;
-  console.log(prodId);
-  res.redirect('/cart');
-  
-}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
