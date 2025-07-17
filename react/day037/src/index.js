acaae3048469f550435a1e321acf68666cd7f299
Piyush Chaturvedi <67586189+PiyushChaturvedii@users.noreply.github.com>
2025-07-17 22:49:33 +0530

    dropdown

---
+++ react/day037/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { ScrollToTop } from "./components";
+import { FilterProvider } from "./context";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <FilterProvider>
+      <ScrollToTop />
+      <App />
+      </FilterProvider>
+    </Router>
+  </React.StrictMode>
+);
