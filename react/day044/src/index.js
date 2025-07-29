58baef3706d9e0c8a83b99456b6dd7f8699ca9de
Piyush Chaturvedi <67586189+PiyushChaturvedii@users.noreply.github.com>
2025-07-29 18:26:22 +0530

    orderpage

---
+++ react/day044/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { ScrollToTop } from "./components";
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FilterProvider, CartProvider } from "./context";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+     <CartProvider>
+       <FilterProvider>
+      <ScrollToTop />
+      <ToastContainer closeButton={false} autoClose={3000} position={"bottom-right"} />
+      <App />
+      </FilterProvider>
+     </CartProvider>
+    </Router>
+  </React.StrictMode>
+);
---
+++ react/day044/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        darkbg: "#1e293b"
+      }
+    },
+  },
+  plugins: [],
+};
